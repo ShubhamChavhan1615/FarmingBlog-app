dfb85d77a31f77e217f4a1a7a80a9bb78be8caac
Shubham_Chavhan <146634527+ShubhamChavhan1615@users.noreply.github.com>
2024-05-13 10:00:04 +0530

    Create About.tsx

---
+++ client/src/Pages/About.tsx
@@ -0,0 +1,14 @@
+import Footer from "../Components/Footer"
+import Navbar from "../Components/Navbar"
+import SubAbout from "../Components/SubAbout"
+function About() {
+  return (
+    <>
+      <Navbar/>
+      <SubAbout/>
+      <Footer/>
+    </>
+  )
+}
+
+export default About
